c5a1b3a5c2a72fb0534aa9020d5b7e42410448ed
QSP <akshay.python@gmail.com>
2021-09-20 12:28:50 +0530

    create project

--- Actitime/src/com/actitime/testscript/ProjectModule.java
+++ Actitime/src/com/actitime/testscript/ProjectModule.java
@@ -1,5 +1,13 @@
 package com.actitime.testscript;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class ProjectModule {
 
+	@Test
+	public void testCreateproject() {
+		Reporter.log("create project",true);
+	}
+	
 }
